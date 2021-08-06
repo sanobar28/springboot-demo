04aba8c877335efcf3bdf61285de81aa94a45f21
sanobar28 <sanobar.mujawar@gmail.com>
2021-08-06 22:50:53 +0530

    [Sanobar]added: Request method get and passed name as path variable

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,21 +1,42 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * RestController mapping request data 
- * to define request handler method.
- * Used to create RESTful web services 
+ * RestController mapping request data to define request handler method. Used to
+ * create RESTful web services
  */
 
 @RestController
+@RequestMapping("/hello")
 public class HelloRestController {
 	
-	@RequestMapping("/")
+	//UC1
+	@RequestMapping(value = { "", "/", "/home" })
 	public String index() {
 		return "Hello from Bridgelabz";
-		
 	}
+
+	/**
+	 * UC2- Request Method Get and passed name as query parameter
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
+	}
+
+	/**
+	 * UC3- Request Method Get and passed name as path variable
+	 * @param name
+	 * @return
+	 */
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+	
+	
 
 }
